dfb0f2618b25503154d5ab6fdde954fd114dd1ea
Ionut Serban <serban1993ionut@gmail.com>
2022-02-21 19:20:23 +0200

    Added Mappings & creation logic

--- ConnectToCRM/GetOrganizationsFromCodeServer.cs
+++ ConnectToCRM/GetOrganizationsFromCodeServer.cs
@@ -18,39 +18,54 @@ using Microsoft.Xrm.Sdk.Messages;
 
 namespace ConnectToCRM
 {
-    public static class CreateCRMConnection
+    public static class GetOrganizationsFromCodeServer
     {
         static ServiceClient service;
-        [FunctionName("CreateCRMConnection")]
+        [FunctionName("GetSoteOrganizations")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            try
+            {
+                  ExecuteJob();
 
-            ExecuteJob();
-            return new OkObjectResult("Connection Succesfull");
+                return new OkObjectResult("Success!");
+            }
+            catch(Exception ex)
+            {
+
+                return new OkObjectResult("ERROR: " + ex.Message);
+            }
         }
 
         public static string ExecuteJob()
         {
-            string result = "Succes";
-            string id = "1.2.246.537.6.202";
-            int pageNo = 1;
-            int totalPages = 0;
-            int count = 0;
-
-            do
+            try
             {
-                result = GetOrganisationsFromCodeserver(id, pageNo);
-                ConceptCodes organisations = JsonConvert.DeserializeObject<ConceptCodes>(result);
-                UpsertToCRM(organisations.ConceptCodes1);
-                totalPages = organisations.TotalPages;
-                pageNo++;
-            } while (pageNo < totalPages);
+                string result = "Succes";
+                string id = "1.2.246.537.6.202";
+                int pageNo = 1;
+                int totalPages = 0;
+                int count = 0;
 
-            return result;
+                do
+                {
+                    result = GetOrganisationsFromCodeserver(id, pageNo);
+                    ConceptCodes organisations = JsonConvert.DeserializeObject<ConceptCodes>(result);
+                    UpsertToCRM(organisations.ConceptCodes1);
+                    totalPages = organisations.TotalPages;
+                    pageNo++;
+                } while (pageNo < totalPages && pageNo == 2);
+
+                return result;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
         }
         public static string GetOrganisationsFromCodeserver(string id, int pageNo)
         {
@@ -91,24 +106,31 @@ namespace ConnectToCRM
             return response;
         }
         public static string UpsertToCRM(ICollection<ConceptCode> organisationsCollection)
-        {
-            string result = "Succes";
+        {            
+            try
+            {
+                List<string> idMainList = organisationsCollection.Select(o => o.ConceptCodeId).ToList();
+                EntityCollection existingCRMRecords = new EntityCollection();
+                var response = ConnectToCRM();
 
-            List<string> idMainList = organisationsCollection.Select(o => o.ConceptCodeId).ToList();
-            EntityCollection existingCRMRecords = new EntityCollection();
-            var response = ConnectToCRM();
+                var existingOrgSubList = GetExistingOrganizations(idMainList, existingCRMRecords);
+                var nonExistingOrgSubList = idMainList.Except(existingOrgSubList).ToList();
 
-            var existingOrgSubList = GetExistingOrganizations(idMainList, existingCRMRecords);
-            var nonExistingOrgSubList = idMainList.Except(existingOrgSubList).ToList();
-
-            ExecuteMultipleRequest exeReq = GetExecuteMultipleReq();
-            CreateNewrecords(organisationsCollection, nonExistingOrgSubList, exeReq);
-            //TODO Add method for Update for existingCRMRecords
-            //TODO ExecuteThe Requests
-            return result;
+                ExecuteMultipleRequest exeReq = GetExecuteMultipleReq();
+                CreateNewrecords(organisationsCollection, nonExistingOrgSubList, exeReq);
+                //TODO Add method for Update for existingCRMRecords
+                //TODO ExecuteThe Requests
+                ExecuteMultipleResponse responseWithResults = (ExecuteMultipleResponse)service.Execute(exeReq);
+                return "Creation Succesfull";
+            }
+            catch(Exception ex)
+            {
+                return "ERROR:" + ex.Message;
+            }
         }
         public static void CreateNewrecords(ICollection<ConceptCode> organisationsCollection, List<string> idList, ExecuteMultipleRequest exeReq)
         {
+            int i = 1;
             foreach (var idStr in idList)
             {
                 IEnumerable<ConceptCode> orgList = organisationsCollection.Where(o => o.ConceptCodeId == idStr);
@@ -117,12 +139,24 @@ namespace ConnectToCRM
 
                 CreateRequest createRequest = new CreateRequest { Target = newCrmOrg };
                 exeReq.Requests.Add(createRequest);
+                if (i == 1)//execute only once
+                    break;
             }
         }
         public static Entity CreateNewCRMRecord(ConceptCode org)
         {
             Entity newOrg = new Entity("els_soteorganisaatiorekisteri");
-            //TODO add attributes dynamically
+            newOrg["els_organizationid"] = org.ClassificationId;
+            newOrg["els_beginningdate"] = org.BeginDate.DateTime;
+            newOrg["els_expiringdate"] = org.ExpirationDate.DateTime;
+            //TODO add attributes from mappings
+            Dictionary<string, string> mappings = GetMappings();
+            foreach(var attr in org.Attributes)
+            {
+                if(mappings.ContainsKey(attr.AttributeName))
+                newOrg[mappings[attr.AttributeName]] = attr.AttributeValue.First();
+            }
+          
             return newOrg;
         }
         public static List<string> GetExistingOrganizations(List<string> idMainList, EntityCollection existingCRMRecords)
@@ -143,7 +177,7 @@ namespace ConnectToCRM
             var query = new QueryExpression("els_soteorganisaatiorekisteri");
             query.ColumnSet.AllColumns = true;
 
-            query.Criteria.AddCondition("els_organizationid", ConditionOperator.In, idMainList);
+            query.Criteria.AddCondition("els_organizationid", ConditionOperator.In, idMainList.ToArray());
             var response = service.RetrieveMultiple(query);
             if (response != null)
             {
@@ -188,6 +222,28 @@ namespace ConnectToCRM
                 return new OkObjectResult("Error:" + ex.Message);
 
             }
+        }
+
+        public static Dictionary<string, string> GetMappings()
+        {
+            Dictionary<string, string> mappings = new Dictionary< string, string>();
+            mappings.Add("Abbreviation", "els_abbreviation");//Item1 = CodeServerField ; Item2= CRMField
+            mappings.Add("LongName", "els_longname");
+            mappings.Add("ParentId", "els_parentid");
+            mappings.Add("PostAddress", "els_postaddress");
+            mappings.Add("StreetAddress", "els_streetaddress");
+            mappings.Add("PostNumber", "els_postnumber");
+            mappings.Add("PostOffice", "els_postoffice");
+            mappings.Add("Sektori", "els_sektori");
+            mappings.Add("Sijainti kunta", "els_sijaintikunta");
+            mappings.Add("Sos.palveluyksikkö", "els_sospalyksikko");
+            mappings.Add("Sos.toimintayksikkö", "sostoimintayksikko");
+            mappings.Add("Terv.palveluyksikkö", "els_tervpalveluyksikko");
+            mappings.Add("TOPI-koodi", "els_topikoodi");
+            mappings.Add("TOPI-nimi", "els_topinimi");
+            mappings.Add("Y-Tunnus", "els_ytunnus");
+
+            return mappings;
         }
     }
 }
